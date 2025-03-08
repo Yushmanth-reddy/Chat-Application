762882086f0aaba025b47db844c66b4214c14dc0
yushmanth <yushmanth@yushmanth-pc.com>
2025-03-08 23:31:00 +0530

    done with update profile pic
    Signed-off-by: yushmanth <yushmanth@yushmanth-pc.com>

--- src/controllers/auth.control.js
+++ src/controllers/auth.control.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -80,6 +81,37 @@ export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successful" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilepic } = req.body;
+    const userId = req.user._id;
+    if (!profilepic) {
+      return res.status(400).json({ message: "Profile pic required" });
+    }
+
+    const uploadresponse = await cloudinary.uploader.upload(profilepic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadresponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: "Internal server Error" });
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import { dbconnect } from "./lib/db.js";
 
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
