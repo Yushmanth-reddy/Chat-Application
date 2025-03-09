64396812cb0a54bc40f7f69f243c7cca9f714b84
yushmanth <yushmanth@yushmanth-pc.com>
2025-03-09 10:16:16 +0530

    done with send message route(need to implement socket.io)
    Signed-off-by: yushmanth <yushmanth@yushmanth-pc.com>

---
+++ src/controllers/message.control.js
@@ -0,0 +1,66 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInuserId = req.user._id;
+
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInuserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const userToChatId = req.params.id;
+    const loggedInuserId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: loggedInuserId, recieverId: userToChatId },
+        { senderId: userToChatId, recieverId: loggedInuserId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const userToChatId = req.params.id;
+    const loggedInuserId = req.user._id;
+
+    let imageURL;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      const imageURL = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId: loggedInuserId,
+      recieverId: userToChatId,
+      text,
+      image: imageURL,
+    });
+
+    await newMessage.save();
+    // implement socket.io
+
+    req.status(200).json(newMessage);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import messageRouter from "./routes/message.route.js";
 import { dbconnect } from "./lib/db.js";
 
 import cookieParser from "cookie-parser";
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/message", messageRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
