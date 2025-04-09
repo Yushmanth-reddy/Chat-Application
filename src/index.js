df77d23c1a78647253a658061734fadca6a8d826
yushmanth <yushmanth@yushmanth-pc.com>
2025-04-09 16:07:47 +0530

    settings done and working on chat section
    Signed-off-by: yushmanth <yushmanth@yushmanth-pc.com>

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
+import cors from "cors";
 import { dbconnect } from "./lib/db.js";
-
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
-app.use(express.json());
+app.use(express.json({ limit: "10mb" })); // increase limit as needed
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
